e239c347454e8a1bbc0ad9ebb440b5bdd936fcc3
Duncan Meech <duncanmeech@gmail.com>
2016-06-27 09:08:34 -0700

    fix order preview test

--- test-e2e/tests/template-order-preview.js
+++ test-e2e/tests/template-order-preview.js
@@ -2,6 +2,8 @@ var homepageRegister = require('../fixtures/homepage-register');
 var openInventory = require('../fixtures/open-inventory');
 var newProject = require('../fixtures/newproject');
 var myProjects = require('../fixtures/myprojects');
+var openTemplatesSample = require('../fixtures/open-templates-sample.js');
+var dragFromTo = require('../fixtures/dragfromto.js');
 
 module.exports = {
   'Verify we can order all templates' : function (browser) {
@@ -9,29 +11,22 @@ module.exports = {
     // maximize for graphical tests
     browser.windowSize('current', 2000, 1100);
     homepageRegister(browser);
+    // example the templates sample project
+    openTemplatesSample(browser);
+    // start a new project
     newProject(browser);
-    myProjects(browser);
+    // drag the first template into the new project
+    dragFromTo(browser, '.InventoryItem-item', 10, 10, '.cvc-drop-target', 10, 10);
+
+    // open the order form
     browser
-      .expect.element('[data-inventory~="project"]:nth-of-type(2)').text.to.equal('EGF Sample Templates');
-    browser
-      .click('[data-inventory~="project"]:nth-of-type(2)')
-      .waitForElementPresent('[data-inventory~="template"]', 5000, 'expected all available templates to appear')
-      .pause(3000)
-      .assert.countelements('.order-button', 29)
-      .click('.construct-viewer:nth-of-type(2) .order-button')
+      .waitForElementPresent('.order-button', 5000, 'expected order button to be available')
+      .click('.order-button')
       .waitForElementPresent('.order-form .page1', 10000, 'expected order dialog to appear')
-      .pause(1000)
-      // multipot, 128 combinations, random selection
-      .setValue('.order-form .row:nth-of-type(2) select', 'Each in an individual container')
-      .pause(1000)
-      .clearValue('.order-form .row:nth-of-type(3) input')
-      .setValue('.order-form .row:nth-of-type(3) input', 128)
-      .pause(1000)
-      .setValue('.order-form .row:nth-of-type(4) select', 'Maximum Unique Set')
       .pause(1000)
       // goto review page
       .click('.buttons + .nav-left-right')
-      .waitForElementPresent('.sceneGraph', 5000, 'expected page 2 with construct viewer')
+      .waitForElementPresent('.sceneGraph', 5000, 'expect a construct viewer / scene graph')
       .waitForElementPresent('[data-nodetype="block"]', 5000, 'expected some blocks')
       // screen shot for image tests
       .saveScreenshot('./test-e2e/current-screenshots/template-order-preview.png')
